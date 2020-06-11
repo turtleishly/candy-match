3e05e5c35c1c30538d5a8ab42ce759a0c37e6457
WJ <WJ@Kees-Air.lan>
2020-06-11 15:49:02 +0800

    done

--- states/Preload.js
+++ states/Preload.js
@@ -1,10 +1,10 @@
 var Match3 = Match3 || {};
 
 Match3.PreloadState = {
-    Preload : function(){
-        this.preloadBar = this.add.Sprite(this.game.world.centerX,this.game.world.centerY,'bar');
+    preload : function(){
+        this.preloadBar = this.add.sprite(this.game.world.centerX,this.game.world.centerY,'bar');
         this.preloadBar.anchor.setTo(0.5);
-        this.preload.Bar.scale.setTo(100,1);
+        this.preloadBar.scale.setTo(100,1);
         this.load.setPreloadSprite(this.preloadBar);
 
         this.load.image('block1','images/bean_blue.png');
@@ -14,7 +14,7 @@ Match3.PreloadState = {
         this.load.image('block5','images/bean_purple.png');
         this.load.image('block6','images/bean_yellow.png');
         this.load.image('block7','images/bean_red.png');
-        this.load.image('block8','images/bean_.whitepng');
+        this.load.image('block8','images/bean_white.png');
         this.load.image('deadBlock','images/bean_dead.png');
         this.load.image('background','images/backyard2.png')
 
